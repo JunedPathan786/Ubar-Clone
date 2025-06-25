1d3bd640f5881af7ed5278f10a1b6e29d34a6a1b
Juned Pathan <junedp068@gmail.com>
2025-06-25 22:46:12 +0530

    ngrok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // important for exposing via ngrok
+    allowedHosts: [
+      "c58f-2409-4081-159f-1009-388d-4b96-bf37-22bc.ngrok-free.app", // your ngrok domain
+    ],
+  },
+});
